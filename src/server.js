e0402f1506b9951a2ca26e71cc7279fc7f96ff29
yevhenii_husak <takerterik@gmail.com>
2025-05-18 15:14:12 +0200

    add

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { envVal } from './utils/envVal.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import contactsRouter from './routers/contacts.js';
 
 const PORT = Number(envVal('PORT', '3000'));
 
@@ -25,52 +27,11 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/contacts', async (req, res, next) => {
-    try {
-      const contacts = await getAllContacts();
-      res.status(200).json({
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-      });
-    } catch (error) {
-      next(error);
-    }
-  });
-
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    try {
-      const { contactId } = req.params;
-      const contact = await getContactById(contactId);
-
-      if (!contact) {
-        return res.status(404).json({ message: 'Contact not found' });
-      }
-
-      res.status(200).json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
-    } catch (error) {
-      next(error);
-    }
-  });
-
-  app.use((req, res) => {
-    console.log(`Time: ${new Date().toLocaleString()}`);
-    res.status(404).json({ message: 'Not found' });
-  });
-
-  app.use((err, req, res, next) => {
-    console.error('Error:', err);
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(contactsRouter);
+  app.use(errorHandler);
+  app.use(notFoundHandler);
 
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on  port  ${PORT}`);
   });
 };
